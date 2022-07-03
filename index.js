6f1088e3ef6beacfffecac853d421b44dee1f0c0
Ojeh Omoniyi <omoniyiojeh95@gmail.com>
2022-07-03 14:33:38 +0100

    added mathematical computation

--- index.js
+++ index.js
@@ -29,8 +29,21 @@ app.post('/split-payments/compute', (req, res) => {
     if (ID == trx.ID && 
         Amount == trx.Amount && 
         Currency == trx.Currency && 
-        CustomerEmail == trx.CustomerEmail ) {
-        res.status(200).json('OK');
+        CustomerEmail == trx.CustomerEmail &&
+        SplitInfo[0].SplitType == trx.SplitInfo[0].SplitType &&
+        SplitInfo[0].SplitValue == trx.SplitInfo[0].SplitValue &&
+        SplitInfo[0].SplitEntityId == trx.SplitInfo[0].SplitEntityId &&
+        SplitInfo[1].SplitType == trx.SplitInfo[1].SplitType &&
+        SplitInfo[1].SplitValue == trx.SplitInfo[1].SplitValue &&
+        SplitInfo[1].SplitEntityId == trx.SplitInfo[1].SplitEntityId) {
+        let initial_balance = Amount;
+        let split_amount1 = SplitInfo[0].SplitValue;
+        let new_balance1 = initial_balance - split_amount1;
+        let split_amount2 = SplitInfo[1].SplitValue;
+        let new_balance2 = new_balance1 - split_amount2;
+        let final_balance = new_balance2;
+
+            // res.status(200).json('OK');
     }
     else {
         res.status(400).json('Bad Request');
